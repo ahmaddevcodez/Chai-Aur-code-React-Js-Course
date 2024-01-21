97d78c02d9ef5b2affdbc082f4e370e8c156462a
ahmaddevcodez <132302808+ahmaddevcodez@users.noreply.github.com>
2024-01-21 17:08:06 +0500

    Your commit message

--- 04bgcolorchanger/src/App.jsx
+++ 04bgcolorchanger/src/App.jsx
@@ -1,11 +1,62 @@
 import { useState } from "react";
+import "./App.css";
+
 function App() {
-  const [Color, setColor] = useState("green");
+  const [color, setColor] = useState();
 
   return (
-    <div className="w-full h-screen" style={{ backgroundColor: Color }}>
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="button-34"
+            role="button"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="button-34"
+            role="button"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="button-34"
+            role="button"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("yellow")}
+            className="button-34"
+            role="button"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+          <button
+            onClick={() => setColor("olive")}
+            className="button-34"
+            role="button"
+            style={{ backgroundColor: "olive" }}
+          >
+            Olive
+          </button>
+          <button
+            onClick={() => setColor("hotpink")}
+            className="button-34"
+            role="button"
+            style={{ backgroundColor: "hotpink" }}
+          >
+            hotpink
+          </button>
+        </div>
       </div>
     </div>
   );
